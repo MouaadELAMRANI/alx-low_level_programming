8311393ab33e164a00eef3e8b921ef193ee40ed4
Mouaad <el.amrani.mouaad@gmail.com>
2023-02-16 12:04:03 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
  * main -Entry point
  *
  * Retir: Always 0 (Succes)
- *
  * */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
