cfd6e085065515e0ac4d906eb08ac2b2e511b5e4
Mouaad <el.amrani.mouaad@gmail.com>
2023-02-23 13:30:04 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *  more_numbers - nmbr
- *   Return: no return
+ * more_numbers - nmbr
+ * Return: no return
  */
 
 void more_numbers(void)
 {
-
 	int i, j;
+
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
