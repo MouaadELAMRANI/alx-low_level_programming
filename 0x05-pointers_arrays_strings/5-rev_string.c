8c0b7a95873727c09e513ed822fd4a4e30696e5d
Mouaad <el.amrani.mouaad@gmail.com>
2023-02-27 13:26:30 -0800

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse
+ * @s: char
+ */
+
+void rev_string(char *s)
+{
+	int = 0, b, c;
+	char d;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	c = a - 1;
+	for (b = 0; c >= 0 && b < c; c--, b++)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+	}
+}
