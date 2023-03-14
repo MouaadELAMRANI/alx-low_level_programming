3cd13c8a4017d8ac1d11c2d1d0ac6cb7130f2cd6
Mouaad <el.amrani.mouaad@gmail.com>
2023-03-14 16:08:54 -0700

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -34,7 +34,7 @@ char **strtow(char *str)
 	if (len == 0)
 		return (NULL);
 
-	words = (char **)malloc(sizof(char *) * (len + 1));
+	words = (char **)malloc(sizeof(char *) * (len + 1));
 	if (words == NULL)
 		return (NULL);
 
