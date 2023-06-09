61ef52f638d32583bc107641b80a805f0ddba9b5
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-09 13:38:58 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - multiply
+ * argc: arg count
+ * argv: arg vect
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
+
+	return (0);
+}
