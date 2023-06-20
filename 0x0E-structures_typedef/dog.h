66da64ccb22fbf2d869c66eb28b7addaf43ad735
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-20 14:56:45 -0700

    task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ * Desc: type dog
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
