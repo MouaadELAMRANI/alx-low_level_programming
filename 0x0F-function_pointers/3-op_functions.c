48a0883f9cc60bd12999f5f76b0db89398d15d6c
Mouaad <el.amrani.mouaad@gmail.com>
2023-06-21 15:10:48 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+/**
+ * op_add - add
+ * @a: int
+ * @b: int
+ * Return: addition
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub
+ * @a: int
+ * @b: int
+ * Return: diff
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multi
+ * @a: int
+ * @b: int
+ * Return: multiplication
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div
+ * @a: int
+ * @b: int
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod
+ * @a: int
+ * @b: int
+ * Return: mod
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
