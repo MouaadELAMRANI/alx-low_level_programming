0d98c0f65fcec1facbd1c12dacfec15900a38e14
Mouaad <el.amrani.mouaad@gmail.com>
2023-07-04 10:46:39 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - elements count
+ * @h: head
+ * Return: count
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
