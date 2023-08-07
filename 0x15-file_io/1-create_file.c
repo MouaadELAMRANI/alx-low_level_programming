de0f0c98ae48b7af5d5744b66a4a680227184778
Mouaad <el.amrani.mouaad@gmail.com>
2023-08-07 15:12:21 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,10 +33,11 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 	{
 		rwr = write(fdesc, text_content, nletters);
-		if (rwr == -1)
-		{
-			return (-1);
-		}
+	}
+
+	if (rwr == -1)
+	{
+		return (-1);
 	}
 
 	close(fdesc);
