600c70e94f4f0fd1b76287c5d4a63a0231701039
MouaadELAMRANI <el.amrani.mouaad@gmail.com>
2023-09-28 15:05:48 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,19 +15,19 @@ unsigned int binary_to_uint(const char *b)
 	{
 		return (0);
 	}
-	for (i = 0; b[i] != "\0"; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != "0" && b[i] != "1")
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
 	}
-	for (i = 0; b[i] != "\0"; i++)
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		j <<= 1;
-		if (b[i] == "1")
+		if (b[i] == '1')
 		{
-			j += 1
+			j += 1;
 		}
 	}
 
